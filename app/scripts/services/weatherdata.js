3e8408cb9b94b97887731ea0519b0b6869f97015
John <john.need@loveopolis.com>
2015-10-12 20:44:25 -0400

    initial commit

---
+++ app/scripts/services/weatherdata.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name weatherApp.weatherData
+ * @description
+ * # weatherData
+ * Service in the weatherApp.
+ */
+angular.module('weatherApp')
+  .service('weatherData', function weatherData() {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+  });
