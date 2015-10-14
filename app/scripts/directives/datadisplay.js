275e388ec34a62e79a9b9bce2309b7b4fccb8968
John <john.need@loveopolis.com>
2015-10-14 06:33:07 -0400

    added server.js

--- app/scripts/directives/datadisplay.js
+++ app/scripts/directives/datadisplay.js
@@ -9,7 +9,7 @@
 angular.module('weatherApp')
   .directive('datadisplay', function () {
     return {
-      templateUrl: '../views/my-weather.html',
+      templateUrl: 'views/my-weather.html',
       restrict: 'E'
       //link: function postLink(scope, element, attrs) {
       //  element.text('this is the dataDisplay directive');
